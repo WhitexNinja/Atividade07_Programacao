0453e8597f613f8d64156fe85b9f1b73a7b9540c
WhitexNinja <152277272+WhitexNinja@users.noreply.github.com>
2025-05-28 12:47:06 -0300

    .

--- previsao_tempo/App.js
+++ previsao_tempo/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+
+  const [weather, setWeather] = useState(null);
+
+  
+
   return (
       <View style={styles.container}>
         <ScrollView>
-          <Text>Open up App.js to start working on your app!</Text>
+          <View style={styles.cartao}>
+            <Text>Teste</Text>
+          </View>
           <StatusBar style="auto" />
         </ScrollView>
       </View>
@@ -16,8 +23,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#2196F3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  cartao: {
+    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+    borderRadius: 16,
+    width: '90%',
+    padding: 24,
+    marginVertical: 12,
+    alignItems: 'center',
+    elevation: 6, // sombra Android
+    shadowColor: '#000', // sombra iOS
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4
+  }
 });
